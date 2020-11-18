a16ed6519cbee118c7c5f50562e94defc896b0b6
Dan Shiloh <daniel.shiloh@ons.gov.uk>
2020-11-18 16:23:43 +0000

    removed unnecessary files

--- carsurvey2.Rcheck/00_pkg_src/carsurvey2/tests/testthat/test-ingest.R
+++
@@ -1,7 +0,0 @@
-test_that("function returns an error if API call unsuccessful", {
-  expect_error(carsurvey2::ingest(token = "faketoken"))
-})
-
-test_that("function returns an error when the hashed data does not match check_hash", {
-  expect_error(carsurvey2:ingest(check_hash = "fakehash"))
-})

0a8794842ac82a2b1fdba8ff4c4d769a15ce9bfc
Shiloh <daniel.shiloh@ons.gov.uk>
2021-02-25 13:15:31 +0000

    Remove draft notice from site

--- build_site.r
+++ build_site.r
@@ -2,20 +2,7 @@
 # This script is designed to be used with the project root as the working directory (../)
 library(magrittr)
 
-# Data ingest and basic cleaning
-# Derive new variables
-# All data steps use function prefixed with data_
-
-API_data <- carsurvey2::ingest()
-if(API_data$status_code != 200) stop("Unsuccessful API request. Status code: ", API_data$status_code, "\n Process Killed.")
-
-carsurvey_data <- carsurvey2::convert_raw(API_data)  %>% 
-                  carsurvey2::tidy_ingest() %>%
-                  carsurvey2::enforce_streaming() %>%
-                  carsurvey2::rename_cols() %>%
-                  carsurvey2::recode_grade() %>% 
-                  carsurvey2::derive_rap_scores() %>%
-                  carsurvey2::derive_code_status() 
+carsurvey_data <- carsurvey2::preprocess()
 
 # Build the site
 navbar <- carsurvey2::render_navbar()

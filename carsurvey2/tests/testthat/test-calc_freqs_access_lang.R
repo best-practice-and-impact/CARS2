51baa5943bc014f1fce10fd2057e45c5a8d7edef
colej <jacob.cole@ons.gov.uk>
2021-03-31 17:32:34 +0100

    test fixes
    fixes for terst-calc_freqs_coding_tools and test-calc_freqs_access_lang

--- carsurvey2/tests/testthat/test-calc_freqs_access_lang.R
+++ carsurvey2/tests/testthat/test-calc_freqs_access_lang.R
@@ -13,18 +13,17 @@ langs <- c(
 
 dummy_data <-  data.frame(available_C = c("Yes", "Don't Know", 1, "Other"),
                           available_SQL = c("No", "Yes", 2, NA),
-                          other_column = c(1,2,3,4),
-                          available_other = c("Yes","Yes",NA,"No") )
+                          other_column = c(1,2,3,4))
 
-expected_output <- data.frame("Programming language" = factor(c("C++ / C#", "other", "SQL"), levels = c("C++ / C#", "other", "SQL")),
-                              "Yes" = as.integer(c(1, 2, 1)), 
-                              "Don't know" = as.integer(c(1, 0, 0)),
-                              "No" = as.integer(c(0, 1, 1)))
+expected_output <- data.frame("Programming language" = factor(c("C++ / C#", "SQL"), levels = c("C++ / C#", "SQL")),
+                              "Yes" = as.integer(c(1, 1)), 
+                              "Don't know" = as.integer(c(1, 0)),
+                              "No" = as.integer(c(0, 1)))
 colnames(expected_output) <- c("Programming language", "Yes", "Don't know", "No")
 
 access_to_languages_dummy  <- carsurvey2::calc_freqs_access_lang(dummy_data,langs)
 rownames(access_to_languages_dummy) <- NULL
 
 test_that("Function checks frequency of programming languages", {
-  expect_identical(access_to_languages_dummy, expected_output)
+  expect_equal(access_to_languages_dummy, expected_output)
 })
--- carsurvey2/tests/testthat/test-calc_freqs_coding_tools.R
+++ carsurvey2/tests/testthat/test-calc_freqs_coding_tools.R
@@ -13,19 +13,18 @@ langs <- c(
 
 dummy_data <- data.frame(status_VBA = c("Access and knowledge", "Access only", "Access only", "Access and knowledge", 1),
                          status_SPSS = c("Access only", NA, 1, "Access and knowledge", "No access or knowledge"),
-                         status_other = c("No access or knowledge", "Knowledge only", "Access only", "Access only", "Access and knowledge"),
                          other = c(1, 2, 3, 4, 5))
 
 
 calc_freqs_coding_tools  <- carsurvey2::calc_freqs_coding_tools(dummy_data,langs)
 rownames(calc_freqs_coding_tools) <- NULL
 
-expected_values <- data.frame("Programming language" = factor(c("other","SPSS","VBA"), levels = c("other","SPSS","VBA")),
-                              "Access only" = as.integer(c(2, 1, 2)),
-                              "Access and knowledge" = as.integer(c(1, 1, 2)),
-                              "Knowledge only" = as.integer(c(1, 0, 0)))
+expected_values <- data.frame("Programming language" = factor(c("SPSS","VBA"), levels = c("SPSS","VBA")),
+                              "Access only" = as.integer(c(1, 2)),
+                              "Access and knowledge" = as.integer(c(1, 2)),
+                              "Knowledge only" = as.integer(c(0, 0)))
 colnames(expected_values) <- c("Programming language", "Access only", "Access and knowledge", "Knowledge only")
 
 test_that("Output matches expected values", {
-  expect_identical(calc_freqs_coding_tools, expected_values)
+  expect_equal(expected_values, calc_freqs_coding_tools)
 })
